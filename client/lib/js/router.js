278fdfec7cff0644de19fffc86e2b094e394a345
nmnguyen93 <nhatminh1993@gmail.com>
2015-10-10 15:51:26 +0800

    no message

--- client/lib/js/router.js
+++ client/lib/js/router.js
@@ -63,11 +63,19 @@ Router.map(function() {
         layoutTemplate: "layoutAdmin"
     });
 
+<<<<<<< Updated upstream
     this.route('classHomepage', {
         path:'/classHomePage',
         template: 'homePage',
         layoutTemplate: "layoutAdmin"
     });
 
+=======
+    this.route('facilityBooking', {
+        path:'/facilityBooking',
+        template: 'facilityBooking',
+        layoutTemplate: "layoutAdmin"
+    });
+>>>>>>> Stashed changes
     
 });
