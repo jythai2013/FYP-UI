9b17ad414972d69f3c628a5dcf8f7f4e311d7d01
Matthew <asdf154@gmail.com>
2016-01-19 06:25:35 +0800

    parsing excel works. left validation and actually creating the accounts

---
+++ server/resetPassword.js
@@ -0,0 +1,10 @@
+
+
+Accounts.emailTemplates.siteName = "Sterling Training Hub";
+Accounts.emailTemplates.from = "Sterling Training Hub <asdf154dev@gmail.com>";
+// Accounts.emailTemplates.enrollAccount.subject = function (user) {
+    // return "Welcome to Awesome Town, " + user.profile.name;
+// };
+Accounts.emailTemplates.resetPassword.subject = function(){
+	return "Reset Password for Sterling Training Hub"
+}
