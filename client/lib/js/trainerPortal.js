41fed74c6dc466fba6fca20e01b1f785f0143986
Matthew <asdf154@gmail.com>
2016-02-18 20:58:39 +0800

    excel generation

--- client/lib/js/trainerPortal.js
+++ client/lib/js/trainerPortal.js
@@ -1,29 +1,29 @@
 Template.trainerUploads.helpers({
 	"trainerOngoingCourses" : function findTrainerOngoingCourses(e) {
 		var coursesTaught = Courses.find().fetch();
-		console.log(coursesTaught);
+		// console.log(coursesTaught);
 		return coursesTaught;
 	},
 	
 	"trainerOngoingClasses" : function findTrainerOngoingCourses(e) {
 		var courseCode = Session.get("trainerOngoingCourseSelected");
-		console.log(courseCode);
+		// console.log(courseCode);
 		var groupsTaught = Groups.find({courseCode:courseCode}).fetch();
-		console.log(groupsTaught);
+		// console.log(groupsTaught);
 		return groupsTaught;
 	}
 });
 
 Template.trainerUploads.onRendered(function(){
-	console.log($("#classId")[0].value);
-	Session.set("trainerOngoingCourseSelected", $("#classId")[0].value);
+	// console.log($("#courseCode")[0].value);
+	Session.set("trainerOngoingCourseSelected", $("#courseCode")[0].value);
 });
 
 Template.trainerUploads.events({
-	"change #classId":function(e){
+	"change #courseCode":function(e){
 		e.preventDefault();
 		Session.set("trainerOngoingCourseSelected", e.target.value);
-		console.log(e.target.value);
+		// console.log(e.target.value);
 	}
 });
 
